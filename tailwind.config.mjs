34a4b632b6968c3061451ce2a4c147077f870915
John C. Waters <john@johncwaters.com>
2024-01-14 10:46:44 -0700

    Update styles and fonts in Header, Layout, and index.astro files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
       primary: "#BF6C3B",
       secondary: "#F2CA99",
-      background: "#F2E3D0",
-      black: "#00080D",
+      background: "#BF9A78",
       green: "#13ce66",
       blue: "#011526",
     },
     fontFamily: {
-      sans: ["Inter", "sans-serif"],
-      serif: ["Merriweather", "serif"],
+      sans: ["Gnuolane", ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: [],
