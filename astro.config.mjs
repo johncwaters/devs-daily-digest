864fe67a3c84345787cef8fa9c8b88d34c08bdd9
John C. Waters <john@johncwaters.com>
2024-01-14 11:23:46 -0700

    Update astro.config.mjs with vercel adapter options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
@@ -7,5 +7,8 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    imageService: true,
+    devImageService: "sharp",
+  }),
 });
